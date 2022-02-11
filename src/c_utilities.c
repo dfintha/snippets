fb41bdada1ed730e46411c7a2a6fe90b0a2770ff
Dénes Fintha <denes.fintha@gmail.com>
2022-02-11 22:14:15 +0100

    Update c_utilities.c
    - string functions added (strtransform, strtrim, strfirst*, strlast*, chany)
    - newline functions added (fprintnl, printnl)
    - helper macros added (MACRO_*, UNUSED, primitive_swap)
    - headers rearranged
    - demonstration code updated with readable output
    - some warnings fixed
    - year updated in header comment

--- src/c_utilities.c
+++ src/c_utilities.c
@@ -1,19 +1,39 @@
 // c_utilities.c
 // Small utility functions that may prove useful in C.
 // Author: Dénes Fintha
-// Year: 2021
+// Year: 2022
 // -------------------------------------------------------------------------- //
 
 // Interface
 
-#include <locale.h>
-#include <math.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+
+#define MACRO_SCOPE_BEGIN   do {
+#define MACRO_SCOPE_END     } while (0)
+#define MACRO_CONCAT(x1, x2) x1##x2
+#define MACRO_XCONCAT_2(x1, x2) MACRO_CONCAT(x1, x2)
+#define MACRO_XCONCAT_3(x1, x2, x3) MACRO_XCONCAT_2(x1, MACRO_XCONCAT_2(x2, x3))
+#define MACRO_XCONCAT_4(x1, x2, x3, x4) MACRO_XCONCAT_2(x1, MACRO_XCONCAT_3(x2, x3, x4))
+#define MACRO_STRINGIFY(x) #x
+#define MACRO_XSTRINGIFY(x) MACRO_STRINGIFY(x)
+
+#if defined(__GNUC__)
+#define UNUSED(...) MACRO_XCONCAT_3(__VA_ARGS__, _unused_line, __LINE__) __attribute__((unused))
+#else
+#define UNUSED(...) MACRO_XCONCAT_3(__VA_ARGS__, _unused_line, __LINE__)
+#endif
+
+#define primitive_swap(a, b, type)              \
+    MACRO_SCOPE_BEGIN                           \
+        type swap##_##a##_##b##_##type = a;     \
+        a = b;                                  \
+        b = swap##_##a##_##b##_##type;          \
+    MACRO_SCOPE_END
+
+typedef int(*char_transformation_fn)(int);
+typedef int(*char_predicate_fn)(int);
 
 const char * boolyn(bool value);
 const char * booltf(bool value);
@@ -27,8 +47,23 @@ char * sizefmt(size_t size);
 bool streq(const char *str1, const char *str2);
 ptrdiff_t strfindc(const char *haystack, int needle);
 ptrdiff_t strfinds(const char *haystack, const char *needle);
+char * strtransform(char *string, char_transformation_fn transformation);
+char * strtrim(char *string, char_predicate_fn predicate);
+size_t strfirst(const char *string, char_predicate_fn predicate);
+size_t strfirstnot(const char *string, char_predicate_fn predicate);
+size_t strlast(const char *string, char_predicate_fn predicate);
+size_t strlastnot(const char *string, char_predicate_fn predicate);
+int chany(char character, const char *set);
+void fprintnl(FILE *stream);
+void printnl(void);
 
 // Implementation
+
+#include <locale.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 const char * boolyn(bool value) {
     return value ? "yes" : "no";
@@ -70,7 +105,7 @@ void shuffle(void *array, size_t size, size_t count) {
     char *bytes = (char *) array;
     for (size_t i = 0; i < count ; ++i) {
         const size_t j = rand() % count;
-        memswap(array + i * size, array + j * size, size);
+        memswap(bytes + i * size, bytes + j * size, size);
     }
 }
 
@@ -120,53 +155,213 @@ ptrdiff_t strfinds(const char *haystack, const char *needle) {
     return where - haystack;
 }
 
+char * strtransform(char *string, char_transformation_fn transformation) {
+    const size_t length = strlen(string);
+    for (size_t i = 0; i < length; ++i) {
+        string[i] = transformation(string[i]);
+    }
+    return string;
+}
+
+char * strtrim(char *string, char_predicate_fn predicate) {
+    const size_t first = strfirstnot(string, predicate);
+    const size_t last = strlastnot(string, predicate);
+    const size_t length = last - first + 1;
+    strncpy(string, string + first, length);
+    string[length] = '\0';
+    return string;
+}
+
+size_t strfirst(const char *string, char_predicate_fn predicate) {
+    const size_t length = strlen(string);
+    size_t where;
+
+    for (where = 0; where < length; ++where) {
+        if (predicate(string[where]))
+            break;
+    }
+
+    return where;
+}
+
+size_t strfirstnot(const char *string, char_predicate_fn predicate) {
+    const size_t length = strlen(string);
+    size_t where;
+
+    for (where = 0; where < length; ++where) {
+        if (!predicate(string[where]))
+            break;
+    }
+
+    return where;
+}
+
+size_t strlast(const char *string, char_predicate_fn predicate) {
+    const size_t length = strlen(string);
+    size_t where;
+
+    for (where = 0; where < length; ++where) {
+        if (predicate(string[length - where - 1]))
+            break;
+    }
+
+    return length - where - 1;
+}
+
+size_t strlastnot(const char *string, char_predicate_fn predicate) {
+    const size_t length = strlen(string);
+    size_t where;
+
+    for (where = 0; where < length; ++where) {
+        if (!predicate(string[length - where - 1]))
+            break;
+    }
+
+    return length - where - 1;
+}
+
+int chany(char character, const char *set) {
+    const size_t length = strlen(set);
+    for (size_t i = 0; i < length; ++i) {
+        if (character == set[i])
+            return 1;
+    }
+    return 0;
+}
+
+void fprintnl(FILE *stream) {
+    fputc('\n', stream);
+}
+
+void printnl(void) {
+    fprintnl(stdout);
+}
+
 // Demonstration
 
-#include <stdio.h>
+#include <ctype.h>
 
-int main(void) {
-    // boolean to string functions
-    printf("%s %s %s %s\n", boolyn(true), boolyn(false), booltf(true), booltf(false));
+static int is_basic_whitespace(int character) {
+    return chany(character, " \t\n");
+}
 
-    // rounding with all floating-point types
-    printf("%f %lf %Lf\n", decroundf(3.1415F, 2), decround(3.1415, 2), decroundl(3.1415L, 2));
+static void print_array(int *array, int length) {
+    fputs("{ ", stdout);
+    for (int i = 0; i < length; ++i) {
+        printf("%d ", array[i]);
+    }
+    putc('}', stdout);
+}
+
+int main(UNUSED(int argc), UNUSED(char **argv)) {
+    puts("BOOLEAN TO STRING FORMATTING");
+    puts("value | boolyn(value) | booltf(value)");
+    puts("------|---------------|--------------");
+    printf("%-5d | %-13s | %-13s\n", true, boolyn(true), booltf(true));
+    printf("%-5d | %-13s | %-13s\n", false, boolyn(false), booltf(false));
+    printnl();
+
+    puts("ROUNDING FLOATS TO N DECIMALS");
+    puts("value     | type of value | decround*(value, 2)");
+    puts("----------|---------------|--------------------");
+    printf("%-9f | %-13s | %-12f\n", 3.1415926535F, "float", decroundf(3.1415926535F, 2));
+    printf("%-9lf | %-13s | %-12lf\n", 3.1415926535, "double", decround(3.1415926535, 2));
+    printf("%-9Lf | %-13s | %-12Lf\n", 3.1415926535L, "long double", decroundl(3.1415926535L, 2));
+    printnl();
 
     char buffer1[10] = "ABCDEFGHI";
     char buffer2[10] = "JKLMNOPQR";
-
-    // memory swapping
-    printf("%s %s\n", buffer1, buffer2);
+    puts("MEMORY CHUNK SWAPPING");
+    puts("buffer1    | buffer2    | state");
+    puts("-----------|------------|------------");
+    printf("%-10s | %-10s | before swap\n", buffer1, buffer2);
     memswap(buffer1, buffer2, sizeof(buffer1));
-    printf("%s %s\n", buffer1, buffer2);
+    printf("%-10s | %-10s | after swap\n", buffer1, buffer2);
+    printnl();
 
-    // successful substring/character index lookup
-    printf("%zd %zd\n", strfinds(buffer1, "MNO"), strfindc(buffer2, 'A'));
+    puts("SUBSTRING AND CHARACTER LOOKUP");
+    puts("buffer     | substring or character | result");
+    puts("-----------|------------------------|-------");
+    printf("%-10s | %-22s | %-6td\n", buffer1, "\"MNO\"", strfinds(buffer1, "MNO"));
+    printf("%-10s | %-22s | %-6td\n", buffer1, "\"ABC\"", strfinds(buffer1, "ABC"));
+    printf("%-10s | %-22s | %-6td\n", buffer1, "'A'", strfindc(buffer1, 'A'));
+    printf("%-10s | %-22s | %-6td\n", buffer1, "'M'", strfindc(buffer1, 'M'));
+    printnl();
 
-    // unsuccessful substring/character index lookup
-    printf("%zd %zd\n", strfinds(buffer1, "ABC"), strfindc(buffer2, 'M'));
+    puts("STRING EQUALITY CHECK");
+    puts("buffer1    | buffer2    | streq(buffer1, buffer2)");
+    puts("-----------|------------|------------------------");
+    printf("%-10s | %-10s | %s\n", buffer1, buffer2, booltf(streq(buffer1, buffer2)));
+    printf("%-10s | %-10s | %s\n", buffer1, buffer1, booltf(streq(buffer1, buffer1)));
+    printnl();
 
-    // string equality check
-    printf("%s %s\n", booltf(streq(buffer1, buffer1)), booltf(streq(buffer1, buffer2)));
-
-    // a memory clearing shall make the buffer a zero-length string
+    puts("MEMORY CLEARING");
+    puts("buffer     | strlen(buffer) | state");
+    puts("-----------|----------------|-------------");
+    printf("%-10s | %-14ld | before clear\n", buffer1, strlen(buffer1));
     memclr(buffer1, sizeof(buffer1));
-    printf("%lu\n", strlen(buffer1));
+    printf("%-10s | %-14ld | after clear\n", buffer1, strlen(buffer1));
+    printnl();
 
-    // size formatting
+    puts("DATA SIZE FORMATTING");
+    puts("value                  | sizefmt(value)");
+    puts("-----------------------|---------------");
     char *formatted = sizefmt(2147483648);
-    printf("%s\n", formatted);
+    printf("%-22lu | %-14s\n", 2147483648, formatted);
     free(formatted);
+    formatted = sizefmt(1024);
+    printf("%-22lu | %-14s\n", 1024UL, formatted);
+    free(formatted);
+    formatted = sizefmt(467445623434543);
+    printf("%-22lu | %-14s\n", 467445623434543, formatted);
+    free(formatted);
+    printnl();
 
-    // array shuffling
     int array[] = { 1, 2, 3, 4, 5 };
-    printf("{ ");
-    for (size_t i = 0; i < sizeof(array) / sizeof(int); ++i)
-        printf("%d ", array[i]);
-    printf("}\n{ ");
+    puts("ARRAY SHUFFLING");
+    puts("array         | state");
+    puts("--------------|---------------");
+    print_array(array, sizeof(array) / sizeof(int));
+    puts(" | before shuffle");
     shuffle(array, sizeof(int), 5);
-    for (size_t i = 0; i < sizeof(array) / sizeof(int); ++i)
-        printf("%d ", array[i]);
-    printf("}\n");
+    print_array(array, sizeof(array) / sizeof(int));
+    puts(" | after shuffle");
+    printnl();
+
+    char hello_str[] = "   hello   ";
+    puts("STRING MANIPULATION");
+    puts("string      | state");
+    puts("------------|----------------------------------");
+    printf("%-11s | original\n", hello_str);
+    printf("%-11s | after trim with whitespace filter\n", strtrim(hello_str, is_basic_whitespace));
+    printf("%-11s | after transform with toupper\n", strtransform(hello_str, toupper));
+    printnl();
+
+    int value1 = 10;
+    int value2 = 20;
+    puts("PRIMITIVE SWAPPING");
+    puts("value1 | value2 | state");
+    puts("-------|--------|------------");
+    printf("%-6d | %-6d | before swap\n", value1, value2);
+    primitive_swap(value1, value2, int);
+    printf("%-6d | %-6d | after swap\n", value1, value2);
+    printnl();
+
+#define X 42
+    puts("STRINGIFICATION");
+    puts("X  | MACRO_STRINGIFY(X) | MACRO_XSTRINGIFY(X)");
+    puts("---|--------------------|--------------------");
+    printf("%-2d | %-18s | %-19s\n", X, MACRO_STRINGIFY(X), MACRO_XSTRINGIFY(X));
+    printnl();
+#undef X
+
+    puts("NOTES");
+    puts(" 1. The UNUSED macro is demonstrated by the compilation yielding");
+    puts("    no warnings for unused variables.");
+    puts(" 2. Functions not explicitly tested are either re-used by another");
+    puts("    function, thus proving they work, or are way too simple or");
+    puts("    similar to other function(s), that demonstrating them is not");
+    puts("    necessary.");
 
     return EXIT_SUCCESS;
 }
