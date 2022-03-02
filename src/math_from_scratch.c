bc0da6798bbc2aceef563ba445db39ab1c264bdc
Dénes Fintha <denes.fintha@gmail.com>
2022-03-02 02:10:15 +0100

    Add square root, cube root, and exponential functions to math_from_scratch.c

--- src/math_from_scratch.c
+++ src/math_from_scratch.c
@@ -18,6 +18,9 @@ double tangent(double x);
 double cotangent(double x);
 double secant(double x);
 double cosecant(double x);
+double square_root(double x);
+double cube_root(double x);
+double exponential(double x);
 
 // Implementation
 
@@ -45,7 +48,7 @@ double factorial(unsigned n) {
 
 double sine(double x) {
     double result = 0.0;
-    for (unsigned n = 0; n < 100; ++n) {
+    for (unsigned n = 0; n < 20; ++n) {
         const double sign = (n % 2 == 0) ? 1 : -1;
         result += (sign * power(x, 2 * n + 1)) / factorial(2 * n + 1);
     }
@@ -54,7 +57,7 @@ double sine(double x) {
 
 double cosine(double x) {
     double result = 0.0;
-    for (unsigned n = 0; n < 100; ++n) {
+    for (unsigned n = 0; n < 20; ++n) {
         const double sign = (n % 2 == 0) ? 1 : -1;
         result += (sign * power(x, 2 * n)) / factorial(2 * n);
     }
@@ -64,7 +67,7 @@ double cosine(double x) {
 double tangent(double x) {
     double numerator = 0.0;
     double denumerator = 0.0;
-    for (unsigned n = 0; n < 100; ++n) {
+    for (unsigned n = 0; n < 20; ++n) {
         const double sign = (n % 2 == 0) ? 1 : -1;
         numerator += (sign * power(x, 2 * n + 1)) / factorial(2 * n + 1);
         denumerator += (sign * power(x, 2 * n)) / factorial(2 * n);
@@ -84,6 +87,31 @@ double cosecant(double x) {
     return 1.0 / sine(x);
 }
 
+double square_root(double x) {
+    double n[5] = { 1.0, 0.0, 0.0, 0.0, 0.0 };
+    for (int i = 1; i < 5; ++i) {
+        n[i] = 0.5 * (n[i - 1] + x / n[i - 1]);
+    }
+    return n[4];
+}
+
+double cube_root(double x) {
+    double n[5] = { 1.0, 0.0, 0.0, 0.0, 0.0 };
+    for (int i = 1; i < 5; ++i) {
+        const double nc = n[i - 1] * n[i - 1] * n[i - 1];
+        n[i] = n[i - 1] * (nc + 2 * x) / (2 * nc + x);
+    }
+    return n[4];
+}
+
+double exponential(double x) {
+    double result = 1.0;
+    for (unsigned i = 1; i < 25; ++i) {
+        result += power(x, i) / factorial(i);
+    }
+    return result;
+}
+
 // Demonstration
 
 #include <stdio.h>
@@ -101,11 +129,14 @@ int main(void) {
 
     printf("x = %.1lf\n", x);
     printf("n = %u\n", n);
-    printf("x^n == %lf == %lf\n", power(x, n), pow(x, n));
-    printf("n! == %.0lf\n", factorial(n));
-    printf("sin(x) == %lf == %lf\n", sine(x), sin(x));
-    printf("cos(x) == %lf == %lf\n", cosine(x), cos(x));
-    printf("tan(x) == %lf == %lf\n", tangent(x), tan(x));
+    printf("    x^n : %lf =?= %lf\n", power(x, n), pow(x, n));
+    printf("     n! : %.0lf\n", factorial(n));
+    printf(" sin(x) : %lf =?= %lf\n", sine(x), sin(x));
+    printf(" cos(x) : %lf =?= %lf\n", cosine(x), cos(x));
+    printf(" tan(x) : %lf =?= %lf\n", tangent(x), tan(x));
+    printf("sqrt(x) : %lf =?= %lf\n", square_root(x), sqrt(x));
+    printf("cbrt(x) : %lf =?= %lf\n", cube_root(x), cbrt(x));
+    printf(" exp(x) : %lf =?= %lf\n", exponential(x), exp(x));
 
     return EXIT_SUCCESS;
 }
