5403326eb3f70c3be44e08abae7a58e18a75425f
Dénes Fintha <denes.fintha@gmail.com>
2024-04-11 00:25:29 +0200

    Simplify character decoding and add a missing header in base64.c

--- src/base64.c
+++ src/base64.c
@@ -9,6 +9,7 @@
 
 // Interface
 
+#include <stddef.h>
 #include <stdint.h>
 
 char * b64encode(const uint8_t *data, size_t length);
@@ -53,21 +54,18 @@ char * b64encode(const uint8_t *data, size_t length) {
 }
 
 static uint8_t decode_map(char c) {
-    static const uint8_t map[] = {
-        'A',  0,  'B',  1,  'C',  2,  'D',  3, 'E',  4,  'F',  5,  'G',  6,  'H',  7,
-        'I',  8,  'J',  9,  'K', 10,  'L', 11, 'M', 12,  'N', 13,  'O', 14,  'P', 15,
-        'Q', 16,  'R', 17,  'S', 18,  'T', 19, 'U', 20,  'V', 21,  'W', 22,  'X', 23,
-        'Y', 24,  'Z', 25,  'a', 26,  'b', 27, 'c', 28,  'd', 29,  'e', 30,  'f', 31,
-        'g', 32,  'h', 33,  'i', 34,  'j', 35, 'k', 36,  'l', 37,  'm', 38,  'n', 39,
-        'o', 40,  'p', 41,  'q', 42,  'r', 43, 's', 44,  't', 45,  'u', 46,  'v', 47,
-        'w', 48,  'x', 49,  'y', 50,  'z', 51, '0', 52,  '1', 53,  '2', 54,  '3', 55,
-        '4', 56,  '5', 57,  '6', 58,  '7', 59, '8', 60,  '9', 61,  '+', 62,  '/', 63,
-        '=',  0
-    };
+    if (c >= 'A' && c <= 'Z')
+        return c - 'A';
 
-    for (size_t i = 0; i < sizeof(map); i += 2) {
-        if ((char) map[i] == c)
-            return map[i + 1];
+    if (c >= 'a' && c <= 'z')
+        return c - 'a' + 26;
+
+    if (c >= '0' && c <= '9')
+        return c - '0' + 52;
+
+    switch (c) {
+        case '+': return 62;
+        case '/': return 63;
     }
 
     return 0;
