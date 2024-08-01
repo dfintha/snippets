cfe9fd7631d50f025a88b41d1e74ed1a894e9bd0
Dénes Fintha <denes.fintha@gmail.com>
2024-08-01 22:47:04 +0200

    Use methods instead of reference members in `namedtuple.cpp`

--- src/namedpair.cpp
+++ src/namedpair.cpp
@@ -8,11 +8,22 @@
 
 #include <utility>
 
-#define namedpair(first_type, first_name, second_type, second_name)     \
-    struct : public std::pair<first_type, second_type> {                \
-        using std::pair<first_type, second_type>::pair;                 \
-        first_type& first_name = this->first;                           \
-        second_type& second_name = this->second;                        \
+#define NAMEDPAIR(FIRST_T, FIRST_NAME, SECOND_T, SECOND_NAME)           \
+    struct : public std::pair<FIRST_T, SECOND_T> {                      \
+        using base = std::pair<FIRST_T, SECOND_T>;                      \
+                                                                        \
+        using std::pair<FIRST_T, SECOND_T>::pair;                       \
+        using std::pair<FIRST_T, SECOND_T>::operator=;                  \
+        using std::pair<FIRST_T, SECOND_T>::swap;                       \
+                                                                        \
+        base& as_pair() { return *this; }                               \
+        const base& as_pair() const { return *this; }                   \
+                                                                        \
+        FIRST_T& FIRST_NAME() { return this->first; }                   \
+        const FIRST_T& FIRST_NAME() const { return this->first; }       \
+                                                                        \
+        SECOND_T& SECOND_NAME() { return this->second; }                \
+        const SECOND_T& SECOND_NAME() const { return this->second; }    \
     }
 
 // Demonstration
@@ -21,8 +32,20 @@
 #include <string>
 
 int main() {
-    using status = namedpair(int, code, std::string, message);
-    const status result{0, "Success"};
-    printf("%s\n", result.message.c_str());
-    return result.code;
+    using status_t = NAMEDPAIR(int, code, std::string, message);
+    const status_t result{0, "Success"};
+
+    printf(
+        "(%d, %s)\n",
+        result.as_pair().first,
+        result.as_pair().second.c_str()
+    );
+
+    printf(
+        "(%d, %s)\n",
+        result.code(),
+        result.message().c_str()
+    );
+
+    return result.code();
 }
